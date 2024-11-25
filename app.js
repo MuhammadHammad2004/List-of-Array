58cfd88c1768eaf50495450ec6a6041523e22b73
Muhammad Hammad <hammadsiddiq666@gmail.com>
2024-11-25 08:26:55 -0800

    Comleted work

---
+++ app.js
@@ -0,0 +1,137 @@
+//  ======= Chap 13======
+
+// var a ="cold";
+// var b="cold";
+
+// if(true)conruesole.log(a===b);
+     
+
+// var a=35;
+// var b=35;
+// var c=5var5;
+// var d=85;
+
+// if(true) console.log(a===b && c===d)
+
+// var a="Hamza";
+// var b=35;
+
+// if(prompt("Nmae")===a && prompt("Age")===b){
+//     console.log(true)
+// }
+
+// var a=33;
+// var data=prompt("Age")
+
+// if(a>data && a<data)alert("ok");
+
+
+// var a="Hammad";
+// var b="Siddiq";
+
+// if(prompt("Name") ==a && prompt("Last Name") == b) alert("ok");
+
+
+// =====Chap 14======
+
+// var a=prompt("Password");
+// var b=5;
+
+// if(a>=b){
+//     alert("OK")
+// }else{
+//     alert("Password must be greater than 5!")
+// }
+
+
+
+// var a=prompt("num");
+// var b=prompt("Name!")
+
+// if(a===1) {
+//     if(b==="Max"){
+//         alert("ok")
+//     }
+// }
+    
+
+// var a=18;
+// var b=18;
+
+// if(a==b){
+//     if(a<=b){
+//         console.log(true)
+//     }
+// }
+
+// var a=["Hammad"];
+
+// var a=["h","i","j","k","l"];
+
+// console.log(a)
+// console.log(a[2])
+
+// var alpha=["h","i","j","k","l","m","n"];
+
+// console.log(alpha.length)
+
+// var arra=["Hammad"];
+
+// arra.push("Siddiq")
+
+// console.log(arra)
+
+//  ===== Chap 16=====
+
+// var cites=["Lahore","Karachi","Islamabad"];
+// cites.push("Punjab");
+// alert("Punjab")
+
+// console.log(cites)
+
+
+
+// var alpha=["h","i","j","k","l","m","n"];
+
+// alpha.pop()
+
+// console.log(alpha)
+
+
+// var alpha=["h","i","j","k","l","m","n"];
+// alpha.push("O",5);
+
+// console.log(alpha)
+
+// var size=["S","M","L","XL","XXL","XXXl"]
+// size.unshift(5,6,2)
+
+
+// var size=["S","M","XL","XXL","XXXl"]
+// var newarr=size.splice(0,3,"S","M","XL")
+
+// console.log(size)
+
+// var animals=["dog","cat","ox","duck","frog"];
+// animals.splice(1,3,"New ani","new ani")
+
+// console.log(animals)
+
+
+// var size=["S","M","L","XL","XXL"];
+// var newsli=size.slice(2,3);
+
+
+// console.log(size)
+// console.log(newsli)
+
+// var animals=["dog","cat","ox","duck","frog"];
+// var newani=animals.slice(2,5)
+
+// console.log(animals)
+// console.log(newani)
+
+for(let i=1;i<=10;i++){
+    console.log("Hammad")
+}
+    
